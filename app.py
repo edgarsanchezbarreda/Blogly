00ff3a20384202adf396586d023c8be673ad3590
Edgar Sanchez <edgar@Edgars-Air.attlocal.net>
2022-04-13 21:17:45 -0700

    completed user and create user form, need to work on separating create user form and finish the details page

--- app.py
+++ app.py
@@ -8,13 +8,32 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'helloworld123'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
+db.create_all()
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    users = User.query.all()
+    return render_template('user_list.html', users = users)
+
+@app.route('/', methods=["POST"])
+def add_user():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(f"/{new_user.id}")
+
+@app.route('/<int:user_id>')
+def user_details(user_id):
+    user = User.query.get(user_id)
+    return render_template('details.html', user = user)
--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 """Models for Blogly."""
 
-from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+default_image_url = 'https://merriam-webster.com/assets/mw/images/article/art-wap-article-main/egg-3442-e1f6463624338504cd021bf23aef8441@1x.jpg'
 
 def connect_db(app):
     db.app = app
@@ -11,7 +12,7 @@ def connect_db(app):
 
 
 class User(db.Model):
-    __tablename__ = "user"
+    __tablename__ = 'user'
 
     id = db.Column (
         db.Integer,
@@ -20,20 +21,24 @@ class User(db.Model):
     )
 
     first_name = db.Column (
-        db.String(30),
+        db.Text,
         nullable = False,
         unique = True
     )
 
     last_name = db.Column (
-        db.String(30),
+        db.Text,
         nullable = False,
         unique = True
     )
 
     image_url = db.Column (
-        db.String,
+        db.Text,
         nullable = False,
-        default = 'https://merriam-webster.com/assets/mw/images/article/art-wap-article-main/egg-3442-e1f6463624338504cd021bf23aef8441@1x.jpg'
-        )
+        default = default_image_url
+    )
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
